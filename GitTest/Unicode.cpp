06d233a43da477a8cee6c77710c127d8af372c03
Heles <heles.hals@gmail.com>
2023-05-23 22:16:11 +0300

    Delete trash

--- GitTest/Unicode.cpp
+++ GitTest/Unicode.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main() 
 {
-<<<<<<< HEAD
+
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-=======
+
 	cout << "Hello from new branch" << endl;//comment
 	cout << "New commit" << endl;//new commit
->>>>>>> NewBranch
+
 
 
 }
